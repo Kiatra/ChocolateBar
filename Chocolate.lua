154d879cbb431704eba7a408a2a0046d350b80b0
Kiatra <9993201+Kiatra@users.noreply.github.com>
2009-01-25 00:23:09 +0000

    initial commit

---
+++ Chocolate.lua
@@ -0,0 +1,192 @@
+﻿
+local LibStub = LibStub
+local ChocolateBar = LibStub("AceAddon-3.0"):NewAddon("ChocolateBar", "AceConsole-3.0", "AceEvent-3.0")
+
+local broker = LibStub("LibDataBroker-1.1")
+
+ChocolateBar.ChocolatePiece = {}
+ChocolateBar.Drag = {}
+local Drag = ChocolateBar.Drag
+local chocolate = ChocolateBar.ChocolatePiece
+ChocolateBar.Bar = {}
+local bars = ChocolateBar.Bar
+local db
+local chocolateObjects = {}
+local dataObjects = {}
+
+--------
+-- utility functions
+--------
+local function Debug(...)
+	if ChocolateBar.db.char.debug then
+	 	local s = "Debug:"
+		for i=1,select("#", ...) do
+			local x = select(i, ...)
+			if(type(x)== "string" or type(x)== "number")then
+					s = s.." "..x
+			else
+				if(x)then
+					s = s.." ".."not a string"
+				else
+					s = s.." ".."nil"
+				end
+			end
+		end
+		DEFAULT_CHAT_FRAME:AddMessage(s)
+	end
+end
+
+function ChocolateBar:Debug(...)
+	Debug(self, ...)
+end
+local Debug = ChocolateBar.Debug
+
+-- RGBToHex code taken with permission from fortress 
+local function RGBToHex(r, g, b)
+	return ("%02x%02x%02x"):format(r*255, g*255, b*255)	
+end
+
+--------
+-- Ace3 callbacks
+--------
+function ChocolateBar:OnInitialize()
+	self:RegisterOptions()
+	self.db.RegisterCallback(self, "OnProfileChanged", "OnProfileChanged")
+	self.db.RegisterCallback(self, "OnProfileCopied", "OnProfileChanged")
+	self.db.RegisterCallback(self, "OnProfileReset", "OnProfileChanged")
+
+	db = self.db.profile
+	bars.ChocolateBar1 = bars:New("ChocolateBar1",db.barSettings["ChocolateBar1"])
+	--db.barSettings["ChocolateBar2"].yoff = -20
+	--bars.ChocolateBar2 = bars:New("ChocolateBar2",db.barSettings["ChocolateBar2"])
+	--bars.ChocolateBar2:Hide();
+	--bars.ChocolateBar2:SetScript("OnLeave", function(self) self:Hide()end)
+	
+	Drag:RegisterFrame(bars.ChocolateBar1)
+	--Drag:RegisterFrame(bars.ChocolateBar2)
+	
+	local jostle = LibStub("LibJostle-3.0-mod")
+	jostle:RegisterTop(bars.ChocolateBar1)
+end
+
+
+function ChocolateBar:OnEnable()
+	for name, obj in broker:DataObjectIterator() do
+		self:LibDataBroker_DataObjectCreated(nil, name, obj, true) --force noupdate on bars
+	end
+	bars.ChocolateBar1:UpdateBar() --update bars here
+
+	broker.RegisterCallback(self, "LibDataBroker_DataObjectCreated")
+end
+
+function ChocolateBar:OnDisable()
+	for name, obj in broker:DataObjectIterator() do
+		self:DisableDataObject(name)
+	end
+end
+
+--------
+-- LDB callbacks
+--------
+function ChocolateBar:LibDataBroker_DataObjectCreated(event, name, obj, noupdate)
+	Debug("Dataobject Registered:", name, obj.text)
+	
+	local t = obj.type
+	if t and (t ~= "data source" and t ~= "launcher") then
+		Debug("Unknown type", t, name)
+		return
+	end
+	
+	if not dataObjects[name] then
+		dataObjects[name] = obj
+	end
+	
+	if db.objSettings[name].enabled then
+		self:EnableDataObject(name, noupdate)
+	end
+	self:AddObjectOptions(name)
+end
+
+function ChocolateBar:EnableDataObject(name, noupdate)
+	local obj = dataObjects[name]
+	local settings = db.objSettings[name]
+	
+	--get bar from setings
+	local barName = settings.barName
+	
+	local t = obj.type
+	-- set default values depending on data source
+	if barName == "" then
+		if t and t == "data source" then
+			barName = "ChocolateBar1"
+			settings.align = "left"
+			settings.showText = true
+		else	
+			--todo bar2
+			barName = "ChocolateBar1"
+			settings.showText = false
+			--settings.align = "left"
+			settings.align = "right"
+		end
+	end
+	obj.name = name
+	
+	settings.barName = barName
+	settings.enabled = true
+	local choco = chocolate:New(name, obj, settings)
+	chocolateObjects[name] = choco
+	
+	Debug(settings.align)
+	bars[barName]:AddChocolatePiece(choco, name,noupdate)
+	broker.RegisterCallback(self, "LibDataBroker_AttributeChanged_"..name, "AttributeChanged")
+end
+
+function ChocolateBar:DisableDataObject(name)
+	--get bar from setings
+	db.objSettings[name].enabled = false
+	local barName = db.objSettings[name].barName 
+	if(not barName or not bars[barName])then
+		--local choco = chocolateObjects[name]
+		--if choco and choco.Hide then
+		--	choco:Hide()
+		--end
+	else
+		--remove frame from bar
+		bars[barName]:EatChocolatePiece(name)
+	end
+end
+
+function ChocolateBar:AttributeChanged(event, name, key, value)
+	--Debug("ChocolateBar:AttributeChanged ",name," key: ", key, value)
+	local settings = db.objSettings[name]
+	if not settings.enabled then 
+		return 
+	end
+	local choco = chocolateObjects[name]
+	choco:Update(choco, key, value)
+end
+
+--call when general bar options change
+function ChocolateBar:UpdateBarOptions(val)
+	bars.ChocolateBar1:UpdateTexture()
+	bars.ChocolateBar1:UpdateColors()
+	bars.ChocolateBar1:UpdateScale()
+end
+
+function ChocolateBar:OnProfileChanged(event, database, newProfileKey)
+	Debug("OnProfileChanged", event, database, newProfileKey)
+	self:UpdateDB(database.profile)
+	db = database.profile
+	self:UpdateBarOptions()
+	for name, obj in pairs(dataObjects) do
+		if db.objSettings[name].enabled then
+			local choco = chocolateObjects[name]
+			choco.settings = db.objSettings[name]
+			self:DisableDataObject(name)
+			self:EnableDataObject(name, true) --no bar update
+		else
+			self:DisableDataObject(name)
+		end
+	end
+	bars.ChocolateBar1:UpdateBar() --update bars here
+end
---
+++ ChocolateBar.lua
@@ -0,0 +1,249 @@
+﻿local ChocolateBar = LibStub("AceAddon-3.0"):GetAddon("ChocolateBar")
+local LSM = LibStub("LibSharedMedia-3.0")
+local Bar = ChocolateBar.Bar
+local chocolate = ChocolateBar.ChocolatePiece
+local Debug = ChocolateBar.Debug
+
+function Bar:New(name, settings)
+	local frame = CreateFrame("Frame",name,UIParent)
+
+	-- add class methods to frame object
+	for k, v in pairs(Bar) do
+		frame[k] = v
+	end
+	
+	frame:SetHeight(settings.height)
+	frame:SetPoint("TOPLEFT",-1,1);
+	--frame:SetPoint("TOPLEFT", settings.xoff, settings.yoff);
+	frame:SetPoint("RIGHT", "UIParent" ,"RIGHT",0, 0);
+	
+	frame:EnableMouse(true)
+	frame:SetScript("OnEnter", function() 
+		ChocolateBar1:SetAlpha(1)
+	end)
+	--frame:SetScript("OnLeave", OnLeave)
+	frame:SetScript("OnLeave", function() 
+		if ChocolateBar.db.profile.hideonleave then
+			ChocolateBar1:SetAlpha(0)
+		end
+	end)
+	
+	frame:SetScript("OnMouseUp", function() 
+		if arg1 == "RightButton" then
+			LibStub("AceConfigDialog-3.0"):Open("ChocolateBar")
+		end
+	end)
+	
+	frame.settings = settings
+	frame:UpdateTexture()
+	frame:UpdateColors()
+	frame:UpdateScale()
+	
+	frame.chocolist = {} --create list of chocolate chocolist in the bar
+	return frame
+end
+
+function Bar:UpdateScale()
+	self.scale = ChocolateBar.db.profile.scale
+	self:SetScale(self.scale)
+end
+
+function Bar:UpdateColors()
+	local bg = ChocolateBar.db.profile.background
+	local color = bg.borderColor
+	self:SetBackdropBorderColor(color.r,color.g,color.b,color.a)
+	color = bg.color
+	self:SetBackdropColor(color.r,color.g,color.b,color.a)
+end
+
+function Bar:UpdateTexture()
+	local background = LSM:Fetch("statusbar", ChocolateBar.db.profile.background.texture)
+	local bg = {
+		bgFile = background, 
+		edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
+		tile = false, tileSize = 16, edgeSize = 12, 
+		--insets = { left = 4, right = 4, top = 4, bottom = 4}
+		insets = { left = 0, right = 0, top = 0, bottom = 0}
+	}
+	bg.bgFile = background
+	self:SetBackdrop(bg);
+end
+
+function GetTexture(frame)
+	Debug(frame:GetName())
+	regions = frame:GetRegions()
+end
+
+-- add some chocolate to a bar
+function Bar:AddChocolatePiece(choco, name,noupdate)
+	local chocolist = self.chocolist
+	if chocolist[name] then
+		return
+	end
+	
+	chocolist[name] = choco
+	
+	choco:SetParent(self)
+	choco.bar = self
+
+	if not noupdate then
+		self:UpdateBar(self)
+	end
+	--self:UpdateChocolte(name, key, value)
+end
+
+-- eat some chocolate from a ChocolateBar
+function Bar:EatChocolatePiece(name)
+	self.chocolist = self.chocolist or {}
+	local choco = self.chocolist[name]
+	
+	if choco then
+		choco:Hide()
+		self.chocolist[name] = nil
+		self:UpdateBar(self)
+	end
+end
+
+function Bar:Drop(choco, pos)
+	Debug("Bar:Drop", choco.name, pos)
+	self.dummy:Hide()
+	self.chocolist[choco.obj.name] = choco
+	Debug("frame:GetWidth() ", choco:GetWidth())
+	choco.settings.index = self.dummy.settings.index
+	choco.settings.align = self.dummy.settings.align
+	self:UpdateBar(true)
+end
+
+function Bar:LoseFocus(name)
+	self.dummy:Hide()
+	self.chocolist[name] = nil
+	self:UpdateBar(true)
+end
+
+function Bar:GetChocolateAtCursor()
+	local s = self:GetEffectiveScale()
+	local x, y = GetCursorPosition()
+	x = x/s
+	for k, v in pairs(self.chocolist) do
+		if x > v:GetLeft() and x < v:GetRight() then
+			return v
+		end
+	end
+	return nil
+end
+
+function Bar:UpdateDragChocolate()
+	local choco = self:GetChocolateAtCursor()
+	if not choco then 
+		Debug("Bar:UpdateDragChocolate(pos) cursour above: nil")
+		self.dummy.settings.index = 500
+		self:UpdateBar()
+	else
+		Debug("cursour above: ",choco.name)
+		if self.last ~= choco then
+			self.last = choco
+			self.dummy.settings.index = choco.settings.index - 0.5
+			self.dummy.settings.align = choco.settings.align
+			self:UpdateBar()
+		end
+	end
+end
+
+function Bar:Drag(name)
+	local choco = self.chocolist[name]
+	if not choco then 
+		if self.saved.name == name then
+			choco = self.saved
+		else
+			Debug("chocolate ", name, "not found in list")
+			return
+		end
+	end
+	
+	local dummy = self.dummy
+	if not dummy then  
+		dummy = CreateFrame("Frame", "ChocolateDummy", self)
+		--dummy:SetAllPoints(chocolate.frame)
+		dummy.name = "dummy"
+		dummy:SetBackdrop({bgFile = "Interface/Tooltips/UI-Tooltip-Background", 
+		edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
+		tile = true, tileSize = 16, edgeSize = 6, 
+		insets = { left = 0, right = 0, top = 0, bottom = 0}})
+		dummy:SetBackdropColor(1,0,0,1)
+		dummy:SetBackdropBorderColor(1,0,0,0)
+		self.dummy = dummy
+	end
+	dummy:Show()
+	dummy:SetWidth(choco:GetWidth())
+	dummy:SetHeight(choco:GetHeight())
+	
+	local settings = {}
+	settings.index = choco.settings.index
+	dummy.settings = settings
+	dummy.settings.align = choco.settings.align
+	templeftchocolate = self.chocolist[name]
+	self.saved = choco
+	self.chocolist[name] = dummy
+	TEST5 = self
+	self:UpdateBar()
+end
+
+local function SortTab(tab)
+	local left = {}
+	local right = {}
+	for k,v in pairs(tab) do
+		local index = v["settings"]["index"]
+		if not index then
+			index = 500
+		end
+		if v.settings.align == "left" then
+			table.insert(left,{k,index})
+		else
+			table.insert(right,{k,index})
+		end
+	end
+	table.sort(left, function(a,b)return a[2] < b[2] end)
+	table.sort(right, function(a,b)return a[2] < b[2] end)
+	return left, right
+end
+
+-- rearange all chocolate chocolist in a given bar
+-- called when chocolates are added, removed, moved
+function Bar:UpdateBar(updateindex)
+	local chocolates =  self.chocolist
+	local templeft, tempright = SortTab(chocolates)
+	
+	local yoff = 0
+	local relative = nil
+	for i, v in ipairs(templeft) do
+		k = v[1]
+		chocolates[k]:ClearAllPoints()
+		if(relative)then
+			chocolates[k]:SetPoint("TOPLEFT",relative,"TOPRIGHT", 0,0)
+		else
+			chocolates[k]:SetPoint("TOPLEFT",self, 6,yoff)
+		end
+		if updateindex then
+			chocolates[k].settings.index = i
+		end
+		relative = chocolates[k]
+	end
+	
+	local relative = nil
+	for i, v in ipairs(tempright) do
+		k = v[1]
+		chocolates[k]:ClearAllPoints()
+		if(relative)then
+			chocolates[k]:SetPoint("TOPRIGHT",relative,"TOPLEFT", 0,0)
+			--list them downwards
+			--chocolates[k]:SetPoint("TOPLEFT",relative,"BOTTOMLEFT", 0,-yoffset)
+		else
+			chocolates[k]:SetPoint("TOPRIGHT",self, 6,yoff)
+		end
+		if updateindex then
+			chocolates[k].settings.index = i
+		end
+		relative = chocolates[k]
+		--Debug("index=",i,k)
+	end
+end
