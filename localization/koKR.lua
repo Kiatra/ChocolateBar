9474e15869c1e3311d3f4cb99195f097b7dde9a0
Kiatra <9993201+Kiatra@users.noreply.github.com>
2009-06-28 05:50:24 +0000

    added koKR locale

---
+++ localization/koKR.lua
@@ -0,0 +1,5 @@
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("ChocolateBar", "koKR")
+if not L then return end
+
+--@localization(locale="koKR", format="lua_additive_table", handle-subnamespaces="concat")@
