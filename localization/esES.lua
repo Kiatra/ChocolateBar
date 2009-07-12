f07d4cbca2fa26a5add88b33c3e5490c6efc4a1d
Kiatra <9993201+Kiatra@users.noreply.github.com>
2009-07-12 22:27:33 +0000

    added locale esES

---
+++ localization/esES.lua
@@ -0,0 +1,5 @@
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("ChocolateBar", "esES")
+if not L then return end
+
+--@localization(locale="esES", format="lua_additive_table", handle-subnamespaces="concat")@
