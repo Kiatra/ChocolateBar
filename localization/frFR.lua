6c3818243e886d6e0de46056d637b181c7cce6c5
zanony <zanony@xan.at>
2009-08-20 17:54:20 +0000

    add frFR localization (translated by Pettigrow)

---
+++ localization/frFR.lua
@@ -0,0 +1,5 @@
+local AceLocale = LibStub:GetLibrary("AceLocale-3.0")
+local L = AceLocale:NewLocale("ChocolateBar", "frFR")
+if not L then return end
+
+--@localization(locale="frFR", format="lua_additive_table", handle-subnamespaces="concat")@
