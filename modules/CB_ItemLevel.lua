05d3b2e86755bad7a510909bde1dee902b1f5e16
Kiatra <9993201+Kiatra@users.noreply.github.com>
2019-07-26 02:38:54 +0200

    Add modules
    Commit unfinished changes

---
+++ modules/CB_ItemLevel.lua
@@ -0,0 +1,24 @@
+﻿-- a LDB object that will show/hide the chocolatebar set in the chocolatebar options
+local LibStub = LibStub
+local L = LibStub("AceLocale-3.0"):GetLocale("ChocolateBar")
+
+local dataobj = LibStub("LibDataBroker-1.1"):NewDataObject("CB_ItemLevel", {
+	type = "data source",
+	icon = "Interface\\AddOns\\ChocolateBar\\pics\\ChocolatePiece",
+	label = "Item Level",
+	text  = "iLevel: ---",
+})
+
+function dataobj:OnTooltipShow()
+	self:AddLine(dataobj.text)
+end
+
+local function unitInventoryChange()
+  local overall, equipped = GetAverageItemLevel()
+	dataobj.text = string.format ("iLvl %s/%s" , overall, equipped)
+end
+
+local frame = CreateFrame("Frame")
+frame:SetScript("OnEvent", unitInventoryChange)
+frame:RegisterEvent("UNIT_INVENTORY_CHANGE")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
---
+++ modules/CB_PlayedTime.lua
@@ -0,0 +1,31 @@
+﻿-- a LDB object that will show/hide the chocolatebar set in the chocolatebar options
+local LibStub = LibStub
+local L = LibStub("AceLocale-3.0"):GetLocale("ChocolateBar")
+local savedPlayedTime = ChocolateBarDB.savedPlayedTime
+
+local dataobj = LibStub("LibDataBroker-1.1"):NewDataObject("PlayedTime", {
+	type = "data source",
+	icon = "Interface\\AddOns\\ChocolateBar\\pics\\ChocolatePiece",
+	label = "Played Time",
+	text  = "---",
+})
+
+function dataobj:OnTooltipShow()
+	self:AddLine("PlayedTime")
+	for k, v in pairs(savedPlayedTime) do
+			self:AddLine(string.format("%s - %s", k, v.total))
+	end
+end
+
+local function getPlayerIdentifier()
+  local _, engClass, _, _, _, name, server = GetPlayerInfoByGUID(UnitGUID("player"))
+	return string.format("%s-%s", name, server)
+end
+
+local function ADDON_LOADED(self, totalTimeInMinutes, timeAtThisLevel)
+	savedPlayedTime[getPlayerIdentifier()].total = totalTimeInMinutes
+end
+
+local frame = CreateFrame("Frame")
+frame:SetScript("OnEvent", TIME_PLAYED_MSG)
+frame:RegisterEvent("TIME_PLAYED_MSG")
